d514cf2c7eec8963ddce005651ef67c15ab7039a
huenguyensw <hue.blue@gmail.com>
2023-05-05 15:31:58 +0200

    commit-structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Playing from './components/playing/Playing';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Playing/>
+      <Footer/>
     </div>
   );
 }
