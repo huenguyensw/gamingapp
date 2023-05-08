57e3ef8ac9f3cd3b47ffe04da600c319f7ee9600
huenguyensw <hue.blue@gmail.com>
2023-05-08 10:46:52 +0200

    add a variable to save game mode

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import History from './components/history/History';
 
 
 function App() {
+  const [gameMode, setGameMode] = useState(null); // 1 for human vs human, 2 for human vs computer
   const [player1, setPlayer1] = useState(''); //show play1's name
   const [player2, setPlayer2] = useState(''); //show play1's name
   const [choiceOfPlay1, setChoiceOfPlay1] = useState(null); //save choice of play1
   const [choiceOfPlay2, setChoiceOfPlay2] = useState(null); //save choice of play2
-  const [choiceOfComputer, setChoiceOfComputer] = useState(null); //save choice of play2
+  const [choiceOfComputer, setChoiceOfComputer] = useState(null); //save choice of Computer
+
   return (
     <div className="App">
       <Header/>
