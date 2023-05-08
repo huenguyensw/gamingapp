a846cc64f0dec40f52b2ea5c269f63450edaf83a
huenguyensw <hue.blue@gmail.com>
2023-05-08 10:34:32 +0200

    commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Playing from './components/playing/Playing';
+import Selection from './components/selection/Selection';
+import History from './components/history/History';
+
 
 function App() {
+  const [player1, setPlayer1] = useState(''); //show play1's name
+  const [player2, setPlayer2] = useState(''); //show play1's name
+  const [choiceOfPlay1, setChoiceOfPlay1] = useState(null); //save choice of play1
+  const [choiceOfPlay2, setChoiceOfPlay2] = useState(null); //save choice of play2
+  const [choiceOfComputer, setChoiceOfComputer] = useState(null); //save choice of play2
   return (
     <div className="App">
       <Header/>
-      <Playing/>
+      <Selection/>
+      <History />
       <Footer/>
     </div>
   );
