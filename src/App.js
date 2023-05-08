03b4494c91920251e6bd3d03e31bd5b73b593564
liza <113092354+11liza@users.noreply.github.com>
2023-05-08 12:03:24 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { useState } from 'react';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Selection from './components/selection/Selection';
-import History from './components/history/History';
-
+import Playing from './components/playing/Playing';
 
 function App() {
-  const [player1, setPlayer1] = useState(''); //show play1's name
-  const [player2, setPlayer2] = useState(''); //show play1's name
-  const [choiceOfPlay1, setChoiceOfPlay1] = useState(null); //save choice of play1
-  const [choiceOfPlay2, setChoiceOfPlay2] = useState(null); //save choice of play2
-  const [choiceOfComputer, setChoiceOfComputer] = useState(null); //save choice of play2
   return (
     <div className="App">
       <Header/>
-      <Selection/>
-      <History />
+      <Playing/>
       <Footer/>
     </div>
   );
