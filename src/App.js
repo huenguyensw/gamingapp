9bcad3921ccdff161bb9cefb9d03c0cba5a5e0a2
Nikola Zovko <nikolajoe95@gmail.com>
2023-05-08 12:21:07 +0200

    step1

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ import History from './components/history/History';
 
 
 function App() {
-  const [gameMode, setGameMode] = useState(null); // 1 for human vs human, 2 for human vs computer
+  const [gameMode, setGameMode] = useState(1); // 1 for human vs human, 2 for human vs computer
   const [player1, setPlayer1] = useState(''); //show play1's name
   const [player2, setPlayer2] = useState(''); //show play1's name
   const [choiceOfPlay1, setChoiceOfPlay1] = useState(null); //save choice of play1
   const [choiceOfPlay2, setChoiceOfPlay2] = useState(null); //save choice of play2
   const [choiceOfComputer, setChoiceOfComputer] = useState(null); //save choice of Computer
+  const fakeResults = [
+    {play1: {choice: 0}, play2: {choice: 1}}, {play1: {choice: 1}, play2: {choice: 0}}, 
+  ]
+    
+  const [results, setResults] = useState(fakeResults); //
+  
 
   return (
     <div className="App">
       <Header/>
       <Selection/>
-      <History />
+     {gameMode == 1? <History player1={player1} player2={player2} results={results}/>: gameMode== 2&& < History player1={player1} player2 = {"Computer"} results ={results} />}
       <Footer/>
     </div>
   );
--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
-const History = () => {
+const History = ({player1, player2, results}) => {
+  console.log(results)
   return (
-    <div>
+    <div className='border-history'>
       {/* I en lista därunder skall de
       senaste matchresultaten, upp till max 10 stycken, redovisas med vinnaren i
       fetstil. (t.ex. ”Gunnar (sten) – Datorn (sax)”). I en rubrik ovan listan skall
       anges vad ställningen är för de resultat som ligger i listan, t.ex. att det är 7 –
       3 till Gunnar över datorn */}
+
+        <h3>History moves:</h3>
+
+        {/* <p>{player1}chose {results} and {player2} chose {results}</p> */}
+        {/* console.log(results) */}
+
+        
+
+
     </div>
   )
 }
