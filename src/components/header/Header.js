9ae18fdf7df2f1995aa6179770381ab1f9de1c5d
liza <113092354+11liza@users.noreply.github.com>
2023-05-09 20:02:49 +0200

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -65,21 +65,22 @@ const Header = ({player1, player2,setPlayer1,setPlayer2,setGameMode,gameMode, up
             <option value="2">Human vs Computer</option>
           </select>
         </label>
-      </div>
-      {/* Render player 1 name input */}
-      <div>
-        <label htmlFor="player1Name">
-          Player 1 name:
+        <label htmlFor="player2Name">
+          Player 2 name:
           <input
             type="text" 
-            id="player1Name"
-            value={player1} 
+            id="player2Name"
+            value={player2} 
             onChange={handlePlayer1NameChange} 
-            aria-label="Enter Player 1 Name"
+            aria-label="Enter Player 2 Name"
             disabled={updatedResult ? true : false}
           />
-        </label>      
-        </div>
+        </label> 
+      </div>
+      {/* Render player 1 name input */}
+      
+             
+        
      
     </div>
   );
