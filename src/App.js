e2032d314e001c2df79e36d2fc33d5fbd4a8f6a6
huenguyensw <hue.blue@gmail.com>
2023-05-09 17:08:37 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import History from './components/history/History';
 
 function App() {
   const [gameMode, setGameMode] = useState(2); // 1 for human vs human, 2 for human vs computer
-  const [player1, setPlayer1] = useState(''); //show play1's name
-  const [player2, setPlayer2] = useState(''); //show play1's name
+  const [player1, setPlayer1] = useState('userA'); //show play1's name
+  const [player2, setPlayer2] = useState('userB'); //show play1's name
   const [updatedResult, setUpdatedResult] = useState(false);
-  const [player1TotalScore, setPlayer1TotalScore] = useState(0);
-  const [player2TotalScore, setPlayer2TotalScore] = useState(0);
+  const [player1TotalScore, setPlayer1TotalScore] = useState(null);
+  const [player2TotalScore, setPlayer2TotalScore] = useState(null);
   
   const [results, setResults] = useState({});
   
@@ -23,7 +23,9 @@ function App() {
     <div className="App">
       <Header player1={player1} player2={player2}  gameMode={gameMode} setGameMode={setGameMode} setPlayer1={setPlayer1} setPlayer2={setPlayer2}/>
       <h1 className='sub-title'> Let's play the game </h1>
-      <Selection player1={player1} player2={player2}  gameMode={gameMode} setGameMode={setGameMode} setResults={setResults} results={results} updatedResult={updatedResult} setUpdatedResult={setUpdatedResult} player1TotalScore={player1TotalScore} player2TotalScore={player2TotalScore}/>
+      <Selection player1={player1} player2={player2}  gameMode={gameMode} setGameMode={setGameMode} 
+      setResults={setResults} results={results} updatedResult={updatedResult} setUpdatedResult={setUpdatedResult}
+       player1TotalScore={player1TotalScore} player2TotalScore={player2TotalScore} setPlayer1TotalScore={setPlayer1TotalScore} setPlayer2TotalScore={setPlayer2TotalScore}/>
       <History results={results} updatedResult={updatedResult} setUpdatedResult={setUpdatedResult} player1TotalScore={player1TotalScore} setPlayer1TotalScore={setPlayer1TotalScore} setPlayer2TotalScore={setPlayer2TotalScore} player2TotalScore={player2TotalScore} />
       <Footer/>
     </div>
