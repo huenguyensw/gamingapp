a87b59b6a3c60e149c1fcf65ad438146dd2f2f32
huenguyensw <hue.blue@gmail.com>
2023-05-11 12:42:31 +0200

    remove exit game

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
 
 function Footer() {
-  const [showConfirmation, setShowConfirmation] = useState(false);
-
-  const handleQuitGameButton = () => {
-    setShowConfirmation(true);
-  };
-
-  const handleCancelQuitGameButton = () => {
-    setShowConfirmation(false);
-  };
-
-  const handleConfirmQuitGameButton = () => {
-    setShowConfirmation(false);
-  };
 
   return (
     <div className='footer'>
-      {/* <button>Quit game</button> */}
       <hr/>
       <p> &#169; Hue, Liza, Nadya och Nikola	</p>
     </div>
