1a69cd90fb798576712f32ef874a38c05f255844
liza <113092354+11liza@users.noreply.github.com>
2023-05-11 15:27:06 +0200

    Test update

--- src/components/header/_Header-test_/header.test.js
+++ src/components/header/_Header-test_/header.test.js
@@ -2,33 +2,38 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../Header';
 
 describe('Header component', () => {
+
+  // Test that the header text is rendered
   test('renders header text', () => {
     render(<Header />);
     const headerElement = screen.getByText(/Rock Paper Scissors/i);
     expect(headerElement).toBeInTheDocument();
   });
 
+  // Parameterized test that checks that player names can be updated
   describe.each([
     ['player1', 'John', 'Jane'],
     ['player2', 'Mary', 'Alice']
   ])('%s name update', (player, initialValue, updatedValue) => {
-    test(`updates ${player} name`, () => {
-      const setPlayer = jest.fn();
+    test(updates ${player} name, () => {
+      const setPlayer = jest.fn(); // Create a mock function to set the player name
       render(<Header {...{ [player]: initialValue, [`set${player}`]: setPlayer }} />);
-      const nameInput = screen.getByLabelText(`Enter ${player.replace(/\d/g, '')} Name`);
+      const nameInput = screen.getByLabelText(Enter ${player.replace(/\d/g, '')} Name);
       fireEvent.change(nameInput, { target: { value: updatedValue } });
-      expect(setPlayer).toHaveBeenCalledWith(updatedValue);
+      expect(setPlayer).toHaveBeenCalledWith(updatedValue); // Verify that the mock function was called with the updated value
     });
   });
 
+  // Test that the game mode can be updated
   test('updates game mode', () => {
-    const setGameMode = jest.fn();
+    const setGameMode = jest.fn(); // Create a mock function to set the game mode
     render(<Header gameMode="1" setGameMode={setGameMode} />);
     const gameModeSelect = screen.getByLabelText(/Select Game Mode/i);
     fireEvent.change(gameModeSelect, { target: { value: '2' } });
-    expect(setGameMode).toHaveBeenCalledWith('2');
+    expect(setGameMode).toHaveBeenCalledWith('2'); // Verify that the mock function was called with the updated game mode
   });
 
+  // Parameterized test that checks that inputs are disabled when playing or when a result has been updated
   test.each([
     ['playing', true],
     ['updatedResult', true]
@@ -42,6 +47,7 @@ describe('Header component', () => {
     expect(gameModeSelect).toBeDisabled();
   });
 
+  // Test that the player 2 name field is rendered based on the game mode
   test('renders player 2 name field based on game mode', () => {
     render(<Header gameMode="1" />);
     const player2NameInput = screen.getByLabelText(/Enter Player 2 Name/i);
