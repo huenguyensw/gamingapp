9547ad2183b29fb9c5a3225bc08a9fe1b45c21fb
huenguyensw <hue.blue@gmail.com>
2023-05-11 19:30:05 +0200

    update test cases

--- src/components/userSelectionForm/_test_/UserSelectionForm.test.js
+++ src/components/userSelectionForm/_test_/UserSelectionForm.test.js
@@ -3,6 +3,12 @@ import UserSelectionForm from "../UserSelectionForm";
 
 
 describe("userSelectionForm", () => {
+    const setChoiceOfPlayer = jest.fn();
+    const currentResult = {current:[]};
+    const setResults = jest.fn();
+    const setUpdatedResult = jest.fn();
+    const setWinner = jest.fn();
+
     test('should render playerName', () => {
         //Given
         const playerName = 'Hanna';
@@ -41,9 +47,38 @@ describe("userSelectionForm", () => {
         expect(iconSectionElement).toBeInTheDocument();
     });
 
-    // Reset the mock after the test
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+    it("should render 'You picked' after player made a choice",()=>{
+        //Given
+        const playerName = "Player 1";
+        const choiceOfPlayer = 1;
+
+        //When
+        render(<UserSelectionForm playerName={playerName} choiceOfPlayer={choiceOfPlayer} />)
+        const iconSectionElement = screen.getByText('You picked')
+
+        //Then
+        expect(iconSectionElement).toBeInTheDocument();
+    })
+
+    it('should call the setChoiceOfPlayer function when player makes a choice',()=>{
+        const playerName = 'Player 1';
+        const results = {};
+        const choiced = {current: 0}
+        render(<UserSelectionForm playerName = {playerName} choiceOfPlayer={null} results={results} currentResult={currentResult} setChoiceOfPlayer={setChoiceOfPlayer} setResults={setResults} choiced={choiced} setUpdatedResult={setUpdatedResult}/>)
+        const iconElement = screen.getByTestId('paperIcon');
+        fireEvent.click(iconElement);
+        expect(setChoiceOfPlayer).toHaveBeenCalledWith(2);
+    })
+    it('should call the setUpdatedResult function when players makes their choice',()=>{
+        const playerName = 'Player 1';
+        const results = {};
+        const choiced = {current: 0};
+        render(<UserSelectionForm playerName = {playerName} choiceOfPlayer={null} results={results} currentResult={currentResult} setChoiceOfPlayer={setChoiceOfPlayer} setResults={setResults} choiced={choiced} setUpdatedResult={setUpdatedResult}
+        gameMode={2} setWinner={setWinner}/>)
+        const iconElement = screen.getByTestId('paperIcon');
+        fireEvent.click(iconElement);
+        expect(setUpdatedResult).toHaveBeenCalledWith(true);
+    })
+
 
 })
